dbd41071c9267e8bb0bf80e4c7a8956a1cef61f2
Olayiwola Ibrahim <olayiwolaibrahim46@gmail.com>
2026-08-30 12:35:38 +0100

    fixing apple pay

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ function NotFound() {
       <div className='flex flex-col items-center gap-3 p-4'>
         {/* <Image src={Logo} alt='Intuitionlabs logo' className='md:w-80 md:h-20 w-36 h-12'/> */}
         <div className='flex flex-col items-center gap-1 text-center'>
-          <h1>404 - Page Not Found</h1>
+          <h1 className='text-5xl font-semibold text-black'>404 - Page Not Found</h1>
           <p>Oops! The page you are looking for does not exist.</p>
           <Link href="/" style={{ color: '#0070f3', textDecoration: 'underline' }}> Go back to the homepage</Link>
         </div>
